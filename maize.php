c03d0ab8d94095181e477e9f8983bd9056735ad9
shadasushanthreddy <42132187+shadasushanthreddy@users.noreply.github.com>
2020-04-24 22:21:01 +0530

    Initial commit

---
+++ maize.php
@@ -0,0 +1,224 @@
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Agro Based Products- Rice</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Agro Based Products<sup>2</sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Home</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Interface
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item active">
+        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-cog"></i>
+          <span>Products</span>
+        </a>
+        <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Products:</h6>
+            <a class="collapse-item " href="rice.php">Rice</a>
+            <a class="collapse-item active" href="maize.php">Maize</a>
+            <a class="collapse-item " href="wheat.php">Wheat</a>
+            <a class="collapse-item " href="groundnut.php">Groundnut</a>
+            <a class="collapse-item " href="sugarcane.php">Sugarcane</a>
+            <a class="collapse-item " href="cotton.php">Cotton</a>
+            <a class="collapse-item " href="ragi.php">Ragi</a>
+            <a class="collapse-item " href="sunflower.php">Sunflower</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+
+            
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Maize</h1>
+
+          <div class="row">
+
+            <div class="col-lg-9">
+
+              <div class="card mt-4">
+                <img class="card-img-top img-fluid" src="https://www.essence.com/wp-content/uploads/2019/02/GettyImages-507457626.jpg?width=1920&height=1080" alt="">
+                <div class="card-body">
+                  <h3 class="card-title">Maize</h3>
+                  <h4>Product Price : <?php
+               $con=mysqli_connect("localhost","root","","agro_product");
+                mysqli_connect("localhost","root","") or die(mysqli_error());
+                mysqli_select_db($con,"agro_product") or die("Cannot connect to database");
+          $query2=mysqli_query($con,"Select product_price from product where product_name='maize'");
+          $row=mysqli_fetch_assoc($query2);
+          printf("%d",$row["product_price"]);
+			  
+?> per kg</h4>
+                </div>
+              </div>
+              <!-- /.card -->
+      
+              <div class="card card-outline-secondary my-4">
+                <div class="card-header">
+                  Inventory Details
+                </div>
+                <div class="card-body">
+                <p>  <?php
+               $con=mysqli_connect("localhost","root","","agro_product");
+                mysqli_connect("localhost","root","") or die(mysqli_error());
+                mysqli_select_db($con,"agro_product") or die("Cannot connect to database");
+          $query2=mysqli_query($con,"Select inv from product where product_name='maize'");
+          $row=mysqli_fetch_assoc($query2);
+          printf("%d",$row["inv"]);
+			  
+?> kgs </p>
+                </div>
+              </div>
+              <div class="card card-outline-secondary my-4">
+                <div class="card-header">
+                  Regular Price Updates
+                </div>
+                <div class="card-body">
+                  <p>Will be updated weekly</p>
+                </div>
+              </div>
+              <!-- /.card -->
+      
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+<!-- Scroll to Top Button--
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
